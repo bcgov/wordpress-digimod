d1a6e6b62c8eaf196dfed8e34acfef1a433236c4
Struk <Alex.Struk@gov.bc.ca>
2023-02-27 16:18:41 -0800

    Digital plan supporting code
    Revision 1

--- multiple-blocks-plugin/blocks/scrollspy/src/edit.js
+++ multiple-blocks-plugin/blocks/scrollspy/src/edit.js
@@ -31,7 +31,8 @@ export default function Edit({ attributes, setAttributes, isSelected }) {
 	return (
 		<div {...useBlockProps()}>
 			<div className='scrollspyContainer'>
-			 <div className='scrollspy'>
+			 <div className='scrollspy' role="navigation" aria-label="on this page">
+				<h2 className="scrollspyOnThisPage h3-heading" aria-hidden="true">On this page:</h2>
 				<ScrollspyNav
 					
 					scrollTargetIds={["mission1", "mission2", "mission3", "mission4"]}
@@ -41,11 +42,11 @@ export default function Edit({ attributes, setAttributes, isSelected }) {
 					scrollElementSelector = '[aria-label="Editor content"]'
 				>
 					<ul>
-						<li><a href="#mission1">Connected services</a></li>
-						<li><a href="#mission2">Digital Trust</a></li>
-						<li><a href="#mission3">Reliable &amp; sustainable technology</a></li>
-						<li><a href="#mission4">Digitally equipped BC Public Service</a></li>
-						<li><a href="#">Download the plan (PDF, 101.7KB)</a></li>
+						<li><a href="#mission1"><span class="scrollSpyLinkContent">Connected services</span></a></li>
+						<li><a href="#mission2"><span class="scrollSpyLinkContent">Digital Trust</span></a></li>
+						<li><a href="#mission3"><span class="scrollSpyLinkContent">Reliable and sustainable technology</span></a></li>
+						<li><a href="#mission4"><span class="scrollSpyLinkContent">Digitally equipped BC Public Service</span></a></li>
+						<li class="scrollSpyButtonWrapper"><a class="scrollSpyButton" href="#"><span class="scrollSpyLinkContent">Download the plan (PDF, 101.7KB)</span></a></li>
 					</ul>
 				</ScrollspyNav>
 			</div>
