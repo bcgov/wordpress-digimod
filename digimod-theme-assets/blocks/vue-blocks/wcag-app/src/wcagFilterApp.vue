08279057156f775494a0a3c74318dac2e491a85d
Nate King <codewise.nate@gmail.com>
2023-12-08 11:27:29 -0800

    Removed Active category

--- digimod-theme-assets/blocks/vue-blocks/wcag-app/src/wcagFilterApp.vue
+++ digimod-theme-assets/blocks/vue-blocks/wcag-app/src/wcagFilterApp.vue
@@ -2,12 +2,13 @@
   <div v-if="uniqueTags.length > 1" class='tag-filter-container'>
     <div class="taxonomy-common_component_category wp-block-post-terms" style="float:left;">
       <div v-for="tag, index in uniqueTags" :key="tag" class="tag-checkbox">
-        <input type="checkbox" :id="'tag-' + index" :value="tag" v-model="selectedTags" class="tag-input" />
-        <label :for="tag" class="tag-label tag" tabindex="0" @click="checkTag(index)"
-          @keydown.enter.prevent="checkTag(index)" role="button" :aria-label="getTagAriaLabel(tag)">
-          {{ tag }}
-        </label>
-
+        <template v-if='tag !== "Active"'>
+          <input type="checkbox" :id="'tag-' + index" :value="tag" v-model="selectedTags" class="tag-input" />
+          <label :for="tag" class="tag-label tag" tabindex="0" @click="checkTag(index)"
+            @keydown.enter.prevent="checkTag(index)" role="button" :aria-label="getTagAriaLabel(tag)">
+            {{ tag }}
+          </label>
+        </template>
       </div>
       <button class="clear-filters" @click="clearFilters" @keydown.enter.prevent='clearFilters'>Reset filters</button>
       <span class='num-available'>{{ filteredPosts.length }} of {{ posts.length }} showing</span>
@@ -27,16 +28,18 @@
               <h3 style="margin-bottom:0;margin-top:var(--wp--preset--spacing--20);"
                 class="has-text-color has-secondary-brand-color is-style-default wp-block-post-title card-title"
                 v-html="post.title.rendered"></h3>
-              
-                <p v-if="post.acf.team_name_ministry" style="margin-top:0;">{{ post.acf.team_name_ministry.value_formated }}</p>
 
-              <p style="font-size:1rem;"><span
-                  class="value">
-                  {{ post.acf.short_description ? post.acf.short_description.value_formated : post.acf.description.value }}
+              <p v-if="post.acf.team_name_ministry" style="margin-top:0;">{{ post.acf.team_name_ministry.value_formated }}
+              </p>
+
+              <p style="font-size:1rem;"><span class="value">
+                  {{ post.acf.short_description ? post.acf.short_description.value_formated : post.acf.description.value
+                  }}
                 </span></p>
-
               <div v-if="post.wcag_tag" class="taxonomy-common_component_category wp-block-post-terms wcag-card-tags">
-                <span v-for="tag in post.wcag_tag" :key="tag" class="tag">{{ tag }}</span>
+                <template v-for="tag in post.wcag_tag" :key="tag">
+                  <span v-if='tag !== "Active"' class="tag">{{ tag }}</span>
+                </template>
               </div>
             </div>
           </a>
@@ -45,91 +48,94 @@
     </div>
   </div>
 
-  <p v-else-if="showMessage" class="no-results" v-show="showMessage" aria-live='polite'>Oops, <strong>no {{ postTypeLabel }} results found</strong>. <a href="#" @click.prevent="clearFilters" @keydown.enter.prevent='clearFilters'>Try resetting your filters</a> and refining your selections.</p>
+  <p v-else-if="showMessage" class="no-results" v-show="showMessage" aria-live='polite'>Oops, <strong>no {{ postTypeLabel
+  }} results found</strong>. <a href="#" @click.prevent="clearFilters" @keydown.enter.prevent='clearFilters'>Try
+      resetting your filters</a> and refining your selections.</p>
 </template>
 
 <script setup>
-  import { ref, onMounted, computed } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 
-  const postType = ref('');
-  const postTypeLabel = ref('');
-  const posts = ref([]);
-  const selectedTags = ref([]);
-  const cssClass = ref('');
-  const columns = ref(3);
-  const showMessage = ref(false);
+const postType = ref('');
+const postTypeLabel = ref('');
+const posts = ref([]);
+const selectedTags = ref([]);
+const cssClass = ref('');
+const columns = ref(3);
+const showMessage = ref(false);
 
-  const fetchData = async () => {
-    const url = `/wp-json/wp/v2/${postType.value}?_embed&per_page=100`;
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error('An error has occurred: ' + response.status);
-      }
-      const postsData = await response.json();
-      posts.value = postsData.map((post) => ({
-        ...post,
-        wcag_tag: post._embedded?.['wp:term']?.flatMap((term) => term.map((t) => t.name)) || [],
-      })) 
+const fetchData = async () => {
+  const url = `/wp-json/wp/v2/${postType.value}?_embed&per_page=100`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('An error has occurred: ' + response.status);
+    }
+    const postsData = await response.json();
+    posts.value = postsData.map((post) => ({
+      ...post,
+      wcag_tag: post._embedded?.['wp:term']?.flatMap((term) => term.map((t) => t.name)) || [],
+    }))
       .slice()
       .sort((a, b) => (a.title.rendered > b.title.rendered) ? 1 : -1);;
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-  const checkTag = (index) => {
-    const tag = uniqueTags.value[index];
-    if (selectedTags.value.includes(tag)) {
-      selectedTags.value = selectedTags.value.filter((selectedTag) => selectedTag !== tag);
-    } else {
-      selectedTags.value.push(tag);
-    }
-  };
+const checkTag = (index) => {
+  const tag = uniqueTags.value[index];
+  if (selectedTags.value.includes(tag)) {
+    selectedTags.value = selectedTags.value.filter((selectedTag) => selectedTag !== tag);
+  } else {
+    selectedTags.value.push(tag);
+  }
+};
 
-  const getTagAriaLabel = (tag) => {
-    return `${tag} filter ${selectedTags.value.includes(tag) ? 'selected' : 'deselected'}`;
-  };
+const getTagAriaLabel = (tag) => {
+  return `${tag} filter ${selectedTags.value.includes(tag) ? 'selected' : 'deselected'}`;
+};
 
-  const clearFilters = () => {
-    selectedTags.value = [];
-  };
+const clearFilters = () => {
+  selectedTags.value = [];
+};
 
-  const uniqueTags = computed(() => [...new Set(posts.value.flatMap((post) => post.wcag_tag || []).sort())]);
-  const filteredPosts = computed(() => {
-    if (!selectedTags.value.length) {
-      return posts.value;
-    } else {
-      console.log('posts:', posts);
-      return posts.value
+const uniqueTags = computed(() => [...new Set(posts.value.flatMap((post) => post.wcag_tag || []).sort())]);
+const filteredPosts = computed(() => {
+  if (!selectedTags.value.length) {
+    return posts.value;
+  } else {
+    console.log('posts:', posts);
+    return posts.value
       .filter((post) =>
         post.wcag_tag && post.wcag_tag.length && selectedTags.value.every((tag) => post.wcag_tag.includes(tag))
-      ) 
+      )
       .slice()
       .sort((a, b) => (a.title.rendered > b.title.rendered) ? 1 : -1);
-    }
-  });
+  }
+});
 
-  onMounted(() => {
+onMounted(() => {
 
-    const appElement = document.getElementById('app');
-    cssClass.value = appElement.getAttribute('class');
-    columns.value = parseInt(appElement.getAttribute('data-columns'));
-    postType.value = appElement.getAttribute('data-post-type');
-    postTypeLabel.value = appElement.getAttribute('data-post-type-label');
+  const appElement = document.getElementById('app');
+  cssClass.value = appElement.getAttribute('class');
+  columns.value = parseInt(appElement.getAttribute('data-columns'));
+  postType.value = appElement.getAttribute('data-post-type');
+  postTypeLabel.value = appElement.getAttribute('data-post-type-label');
 
-    fetchData();
+  fetchData();
 
-    setTimeout(() => {
-      showMessage.value = true;
-    }, 3000);
-  });
+  setTimeout(() => {
+    showMessage.value = true;
+  }, 3000);
+});
 </script>
 
 <style scoped>
 .tag-filter-container {
   margin: 2rem 0.33rem 2rem 0;
   display: flex;
+  width: 100%;
 }
 
 .tag-checkbox {
@@ -175,6 +181,7 @@
   overflow: hidden;
   font-size: 1rem;
 }
+
 .clear-filters:hover,
 .clear-filters:focus-visible {
   outline: 2px solid var(--wp--preset--color--gray-80);
@@ -207,7 +214,7 @@
   padding: 2rem;
 }
 
-.wcag-card-content > * {
+.wcag-card-content>* {
   margin-left: 0 !important;
   margin-right: 0 !important;
 }
