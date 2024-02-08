a572a75ece89d31b91f53aa427802358c62568c1
Nicolas Gomez <codewise.nicolas@gmail.com>
2024-02-08 11:47:31 -0800

    Changes to restrictpagesplugin - Added update check functionality - Added function to check if a url or current page is in the list of restricted pages. - Refactored function names to follow plugin name convenction - Augmented main check/redirect function to accept a url

--- restrictpagesplugin/custom-redirect-to-login.php
+++ restrictpagesplugin/custom-redirect-to-login.php
@@ -2,20 +2,39 @@
 /**
  * Plugin Name: Custom Redirect to Login
  * Description: A custom plugin that redirects users to the login URL when they access certain pages.
- * Version: 1.1
+ * Version: 1.2.0
  * Author: Digimod
- * Author URI: https://yourwebsite.com
+ * Author URI: https://digital.gov.bc.ca
  * License: GPL2
+ * Repository: https://github.com/bcgov/wordpress-digimod/tree/main/restrictpagesplugin
+ * Plugin URI: https://github.com/bcgov/wordpress-digimod/tree/main/restrictpagesplugin
+ * Update URI: https://raw.githubusercontent.com/bcgov/wordpress-digimod/main/restrictpagesplugin/custom-redirect-to-login.php
  */
-function custom_redirect_to_login() {
+
+ /** 
+  * Function used to check and redirect users to the login page for restricted pages.
+  *  This function will handle the redirection.
+  *  Logged in users are NOT restricted.
+  *
+  * @param string $url_to_check (optional) The url to be checked, otherwise it uses $_SERVER["REQUEST_URI"]
+  */
+function custom_redirect_to_login($url_to_check = null) {
     // Get the restricted URLs from the saved settings
     $restricted_urls = get_option('custom_restricted_urls', '');
 
     // Convert the saved URLs to an array
     $restricted_page_urls = array_filter(array_map('trim', explode(PHP_EOL, $restricted_urls)));
 
-    // Get the current page URL path
-    $current_page_url = strtok($_SERVER["REQUEST_URI"], '?');
+    //Allow for the function to be passed a url, or use default value.
+    if($url_to_check){
+        $tmp_url = parse_url($url_to_check);
+        $url_to_check = $tmp_url['path'];
+        
+    }else{
+        // Get the current page URL path
+        $url_to_check = $_SERVER["REQUEST_URI"];
+    }
+    $current_page_url = strtok($url_to_check, '?');
 
     // Check if the current page is restricted and the user is not logged in
     if (in_array($current_page_url, $restricted_page_urls) && !is_user_logged_in()) {
@@ -35,42 +54,70 @@ function custom_redirect_to_login() {
         exit;
     }
 }
- 
-
 add_action('template_redirect', 'custom_redirect_to_login');
 
 
-function custom_check_plugin_working() {
+ /** 
+  * Check if a passed in URL (or current url) is in the restricted URL list.
+  *
+  * @param string $url_to_check (optional) The url to be checked, otherwise it uses $_SERVER["REQUEST_URI"]
+  * @return bool Whether the URL is in the list or not.
+  */
+function custom_redirect_to_login_check_if_url_in_list($url_to_check = null){
+    // Get the restricted URLs from the saved settings
+    $restricted_urls = get_option('custom_restricted_urls', '');
+
+    // Convert the saved URLs to an array
+    $restricted_page_urls = array_filter(array_map('trim', explode(PHP_EOL, $restricted_urls)));
+
+    if($url_to_check){
+        $tmp_url = parse_url($url_to_check);
+        $url_to_check = $tmp_url['path'];
+
+    }else{
+        $url_to_check = $_SERVER["REQUEST_URI"];
+    }
+    // Get the current page URL path
+    $current_page_url = strtok($url_to_check, '?');
+
+    // Check if the page is restricted 
+    if (in_array($current_page_url, $restricted_page_urls) ) {  
+        return true;
+    }
+
+    return false;
+}
+
+
+function custom_redirect_to_login_plugin_working() {
     echo '<!-- Custom Redirect to Login plugin is active and working -->';
 }
 
-function custom_hide_admin_bar_for_non_editors() {
-	
-				
+function custom_redirect_to_login_hide_admin_bar_for_non_editors() {				
     if (is_user_logged_in()) {
         $current_user = wp_get_current_user();
         if (!in_array('editor', (array) $current_user->roles) && !in_array('administrator', (array) $current_user->roles)) {
-			add_action('wp_footer', 'custom_check_plugin_working');
+			add_action('wp_footer', 'custom_redirect_to_login_plugin_working');
             show_admin_bar(false);
         }
     }
 }
+add_action('init', 'custom_redirect_to_login_hide_admin_bar_for_non_editors');
 
-add_action('init', 'custom_hide_admin_bar_for_non_editors');
 
-function custom_restricted_urls_menu() {
+function custom_redirect_to_login_urls_menu() {
     add_options_page(
         'Restricted URLs',
         'Restricted URLs',
         'manage_options',
         'custom-restricted-urls',
-        'custom_restricted_urls_settings_page'
+        'custom_redirect_to_login_restricted_urls_settings_page'
     );
 }
+add_action('admin_menu', 'custom_redirect_to_login_urls_menu');
 
-add_action('admin_menu', 'custom_restricted_urls_menu');
 
-function custom_restricted_urls_settings_page() {
+function custom_redirect_to_login_restricted_urls_settings_page() {
     // Check user capabilities
     if (!current_user_can('manage_options')) {
         return;
@@ -105,3 +152,15 @@ function custom_restricted_urls_settings_page() {
     </div>
     <?php
 }
+
+
+
+// Begin function to check for updates to plugin
+require_once "digimod-update-check.php";
+function custom_redirect_to_login_update_check_init(){
+    if (class_exists('digimod_plugin_update_check')) {
+        new digimod_plugin_update_check(__FILE__, plugin_basename(__FILE__));
+    }
+}
+add_action('init', 'custom_redirect_to_login_update_check_init');
+//End update check code.
