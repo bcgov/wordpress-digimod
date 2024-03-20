579449e9994bd4c347cbc0c15ba6f8f0c563708b
Nicolas Gomez <nicolas.gomez@gov.bc.ca>
2024-03-20 00:02:00 -0700

    cleanup and better logging

--- .github/actions/create_wp_instance/deploy.sh
+++ .github/actions/create_wp_instance/deploy.sh
@@ -35,20 +35,26 @@ oc login $OPENSHIFT_SERVER --token=$token --insecure-skip-tls-verify=true
 #Go into the deployment folder
 cd wordpress-deploy-digimod
 
-# Delete existing deployment, if it exists
+#Setup some variables
 export NAMESPACE="c0cce6-$ENVIRONMENT"
 export OC_ENV=$ENVIRONMENT
 export OC_SITE_NAME=digital-$SITE_NAME
+
+# Delete existing deployment, if it exists
+echo "::group::Delete existing deployment"
 export WORDPRESS_POD_NAME=$(oc get pods -n $NAMESPACE -l app=wordpress,role=wordpress-core,site=${OC_SITE_NAME} -o jsonpath='{.items[0].metadata.name}')
 WORDPRESS_CONTAINER_NAME=$(oc get pods -n $NAMESPACE $WORDPRESS_POD_NAME -o jsonpath='{.spec.containers[0].name}')
 if [ -n "$WORDPRESS_CONTAINER_NAME" ]; then
     chmod +x site-delete-unix.sh
     ./site-delete-unix.sh
 fi      
+echo "::endgroup::"
 
 # Create new WordPress instance
+echo "::group::Create new deployment"
 chmod +x site-builder-unix.sh
 ./site-builder-unix.sh
+echo "::endgroup::"
 
 # Wait for WordPress pod to be running
 
@@ -68,7 +74,9 @@ done
 echo "Waiting for WordPress container to be fully initialized..."
 sleep 15
 
+
 # Install wordpress
+echo "::group::Install wordpress"
 # Download wp-cli in the GitHub Actions workspace
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
@@ -78,4 +86,10 @@ oc cp --no-preserve wp-cli.phar $NAMESPACE/$WORDPRESS_POD_NAME:/tmp/wp-cli.phar 
 oc exec -n $NAMESPACE -c $WORDPRESS_CONTAINER_NAME $WORDPRESS_POD_NAME -- chmod +x /tmp/wp-cli.phar
 
 #Perform a site install
-oc exec -n $NAMESPACE -c $WORDPRESS_CONTAINER_NAME $WORDPRESS_POD_NAME -- php /tmp/wp-cli.phar core install --url=${OC_SITE_NAME}.apps.silver.devops.gov.bc.ca --admin_user=tester --admin_email=info@example.com  --title="${OC_SITE_NAME}.gov.bc.ca Testing Framework"
+WP_INSTALL_RESULTS=$(oc exec -n $NAMESPACE -c $WORDPRESS_CONTAINER_NAME $WORDPRESS_POD_NAME -- php /tmp/wp-cli.phar core install --url=${OC_SITE_NAME}.apps.silver.devops.gov.bc.ca --admin_user=tester --admin_email=info@example.com  --title="${OC_SITE_NAME}.gov.bc.ca Testing Framework")
+
+#Disable site indexing
+oc exec -n $NAMESPACE -c $WORDPRESS_CONTAINER_NAME $WORDPRESS_POD_NAME -- php /tmp/wp-cli.phar option set blog_public 0
+echo "::endgroup::"
+
+echo "### WP Install Results: ${WP_INSTALL_RESULTS}" >> $GITHUB_STEP_SUMMARY
