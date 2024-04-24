d63f54415facd739530ad5b55529a85cfec1eb8b
Nicolas Gomez <nicolas.gomez@gov.bc.ca>
2024-04-24 11:20:25 -0700

    digimod-theme-assets - added a warning message to the admin searchwp settings to disable the quote search toggle.

--- digimod-theme-assets/Src/admin.js
+++ digimod-theme-assets/Src/admin.js
@@ -1,2 +1,30 @@
 
 import '../blocks/search-results/index.js';
+
+
+
+const domReady = () => {
+	// Search for and disabled the SearchWP quoted searches toggle. It causes some issue with metrics link tracking leading to a timeout when following links.
+	//	Quoted search will continue to work even if turned off.
+	const isSearchWPSettings = document.querySelector('body.searchwp_page_searchwp-settings');
+	if(isSearchWPSettings){
+		const quotedSearchesToggle = document.querySelector('#swp-quoted_search_support');
+		if(quotedSearchesToggle){
+			quotedSearchesToggle.setAttribute('disabled','disabled');
+
+			let tmpWarning = document.createElement('p');
+			tmpWarning.style.fontWeight = 'bold';
+			tmpWarning.textContent = 'On the DigitalGov Website this must remain off to prevent issues with metrics tracking. Quoted searches will still work even with this disabled.';
+			
+			quotedSearchesToggle.parentNode.parentNode.appendChild(tmpWarning);
+		}
+	}
+	
+}
+
+
+if ('complete' === document.readyState) {
+    domReady();
+} else {
+    document.addEventListener('DOMContentLoaded', domReady);
+}
--- digimod-theme-assets/dist-plugin/admin.asset.php
+++ digimod-theme-assets/dist-plugin/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'adead0f7bda1fccd96b5');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-server-side-render'), 'version' => 'd11e293aaf578a94f229');
