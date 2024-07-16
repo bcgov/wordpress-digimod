0102aa76328ea18cd0a8a194db2f4b5bd2e2db79
Nate King <Nate.King@gov.bc.ca>
2024-07-16 14:55:11 -0700

    Refactored mega menu

--- digimod-theme-assets/scripts/public/mega-menu.js
+++ digimod-theme-assets/scripts/public/mega-menu.js
@@ -6,84 +6,95 @@
  * @return {void}
  */
 const domReady = () => {
-    /*
-     * SafarIE bug requires 0ms timeout.
+    /**
+     * Handles pointer enter event for primary menu items.
+     * @param {HTMLElement} item - The menu item element.
      */
-    setTimeout(function () {
-        /**
-         * This handles the behavior of WordPress navigation menu and submenus as a mega menu.
-         */
+    const handlePrimaryPointerEnter = (item) => {
+        const subMenuContainer = item.querySelector('header .wp-block-navigation__submenu-container');
+        const popularContentShowing = document.querySelector('.live-search-extra');
+
+        if (subMenuContainer && !popularContentShowing) {
+            const firstSubMenuItemButton = subMenuContainer.querySelector('.wp-block-navigation-item:first-child button');
+
+            if (firstSubMenuItemButton && firstSubMenuItemButton.getAttribute('aria-expanded') === 'false') {
+                firstSubMenuItemButton.focus();
+                firstSubMenuItemButton.click();
+                firstSubMenuItemButton.blur();
+            }
+        }
+
+        const itemMenuSubUL = item.querySelector('header .wp-block-navigation__submenu-container .wp-block-navigation__submenu-container');
+        if (itemMenuSubUL && !itemMenuSubUL.isCheckedForHeight) {
+            const parentULContainer = itemMenuSubUL.parentNode.parentNode;
+
+            if (parentULContainer.clientHeight <= itemMenuSubUL.clientHeight) {
+                parentULContainer.style.height = `${itemMenuSubUL.clientHeight}px`;
+            }
+
+            itemMenuSubUL.isCheckedForHeight = true;
+        }
+    };
+
+    /**
+     * Handles pointer leave event for primary menu items.
+     * @param {HTMLElement} item - The menu item element.
+     */
+    const handlePrimaryPointerLeave = (item) => {
+        const subMenuContainer = item.querySelector('header .wp-block-navigation__submenu-container');
+        const firstSubMenuItemButton = subMenuContainer.querySelector('.wp-block-navigation-item:first-child button');
+
+        if (firstSubMenuItemButton) {
+            firstSubMenuItemButton.setAttribute('aria-expanded', 'false');
+        }
+    };
+
+    /**
+     * Initializes primary menu items with event listeners.
+     */
+    const initializePrimaryMenuItems = () => {
         const primaryMenuListItems = document.querySelectorAll(
             'header nav > .wp-block-navigation__container > .wp-block-navigation-item.has-child'
         );
 
         if (primaryMenuListItems.length > 0) {
             primaryMenuListItems.forEach((item) => {
-                const subMenuContainer = item.querySelector(
-                    'header .wp-block-navigation__submenu-container'
-                );
-
-                let isCheckedForHeight = false;
-
-                item.addEventListener('pointerenter', () => {
-                    const popularContentShowing = document.querySelector('.live-search-extra');
-
-                    if (subMenuContainer && ! popularContentShowing) {
-                        const firstSubMenuItemButton =
-                            subMenuContainer.querySelector(
-                                '.wp-block-navigation-item:first-child button'
-                            );
-
-                        if (
-                            firstSubMenuItemButton &&
-                            firstSubMenuItemButton.getAttribute(
-                                'aria-expanded'
-                            ) === 'false'
-                        ) {
-                            firstSubMenuItemButton.focus();
-                            firstSubMenuItemButton.click();
-                            firstSubMenuItemButton.blur();
-                        }
-                    }
-
-                    const itemMenuSubUL = item.querySelector(
-                        'header .wp-block-navigation__submenu-container .wp-block-navigation__submenu-container'
-                    );
-
-                    if (itemMenuSubUL && !isCheckedForHeight) {
-                        const parentULContainer =
-                            itemMenuSubUL.parentNode.parentNode;
-
-                        if (
-                            parentULContainer.clientHeight <=
-                            itemMenuSubUL.clientHeight
-                        ) {
-                            parentULContainer.style.height = `${itemMenuSubUL.clientHeight}px`;
-                        }
-
-                        isCheckedForHeight = true;
-                    }
-                });
-
-                item.addEventListener('pointerleave', () => {
-                    const firstSubMenuItemButton =
-                        subMenuContainer.querySelector(
-                            '.wp-block-navigation-item:first-child button'
-                        );
-
-                    if (firstSubMenuItemButton) {
-                        firstSubMenuItemButton.setAttribute(
-                            'aria-expanded',
-                            'false'
-                        );
-                    }
-                });
+                item.addEventListener('pointerenter', () => handlePrimaryPointerEnter(item));
+                item.addEventListener('pointerleave', () => handlePrimaryPointerLeave(item));
             });
         }
+    };
 
-        const menuSubUL = document.querySelectorAll(
-            'header .wp-block-navigation__submenu-container .wp-block-navigation__submenu-container'
-        );
+    /**
+     * Handles pointer enter event for secondary menu items.
+     * @param {HTMLElement} parentLI - The parent list item element.
+     */
+    const handleSecondaryPointerEnter = (parentLI) => {
+        const menuItemMenuSubUL = parentLI.querySelector('header .wp-block-navigation__submenu-container .wp-block-navigation__submenu-container');
+        menuItemMenuSubUL.style.width = '66vw';
+
+        const parentSubULContainer = menuItemMenuSubUL.parentNode.parentNode;
+        const mainMenuContainer = parentSubULContainer;
+        const subMenuListContainer = menuItemMenuSubUL;
+
+        if (menuItemMenuSubUL && !subMenuListContainer.isSubULCheckedForHeight) {
+            if (mainMenuContainer.clientHeight <= subMenuListContainer.clientHeight) {
+                mainMenuContainer.style.height = `${subMenuListContainer.clientHeight}px`;
+                menuItemMenuSubUL.style.height = 'calc(100% - 3rem)';
+            } else {
+                mainMenuContainer.style.height = `${mainMenuContainer.clientHeight}px`;
+                menuItemMenuSubUL.style.height = '100%';
+            }
+
+            subMenuListContainer.isSubULCheckedForHeight = true;
+        }
+    };
+
+    /**
+     * Initializes secondary menu items with event listeners and headline elements.
+     */
+    const initializeSecondaryMenuItems = () => {
+        const menuSubUL = document.querySelectorAll('header .wp-block-navigation__submenu-container .wp-block-navigation__submenu-container');
 
         if (menuSubUL.length > 0) {
             menuSubUL.forEach((menuItem) => {
@@ -93,81 +104,67 @@ const domReady = () => {
                 const link = document.createElement('a');
                 link.href = parentLI.firstChild.href;
                 link.textContent = parentLI.firstChild.textContent;
-                link.textContent = parentLI.firstChild.textContent;
                 headline.appendChild(link);
                 menuItem.prepend(headline);
                 menuItem.style.width = '66vw';
 
-                let isSubULCheckedForHeight = false;
-
-                parentLI.addEventListener('pointerenter', () => {
-                    const menuItemMenuSubUL = parentLI.querySelector(
-                        'header .wp-block-navigation__submenu-container .wp-block-navigation__submenu-container'
-                    );
-
-                    menuItemMenuSubUL.style.width = '66vw';
-
-                    const parentSubULContainer =
-                        menuItemMenuSubUL.parentNode.parentNode;
-                    const mainMenuContainer = parentSubULContainer;
-                    const subMenuListContainer = menuItemMenuSubUL;
-
-                    if (menuItemMenuSubUL && !isSubULCheckedForHeight) {
-                        if (
-                            mainMenuContainer.clientHeight <=
-                            subMenuListContainer.clientHeight
-                        ) {
-                            mainMenuContainer.style.height = `${subMenuListContainer.clientHeight}px`;
-                            menuItemMenuSubUL.style.height =
-                                'calc(100% - 3rem)';
-                        } else {
-                            mainMenuContainer.style.height = `${mainMenuContainer.clientHeight}px`;
-                            menuItemMenuSubUL.style.height = '100%';
-                        }
-
-                        isSubULCheckedForHeight = true;
-                    }
-                });
+                parentLI.addEventListener('pointerenter', () => handleSecondaryPointerEnter(parentLI));
             });
         }
+    };
 
-        setTimeout(function () {
+    /**
+     * Handles pointer enter and leave events for secondary level links.
+     * @param {HTMLElement} secondaryLink - The secondary link element.
+     */
+    const handleSecondaryLinkPointer = (secondaryLink) => {
+        secondaryLink.addEventListener('pointerenter', (e) => {
+            const theLink = e.target;
+            const topSiblingContainer = theLink.closest('ul');
+            const topSibling = topSiblingContainer.querySelector('li');
+            const topSiblingSubmenu = topSibling.querySelector('ul');
+            if (topSiblingSubmenu) {
+                topSiblingSubmenu.style.display = 'none';
+            }
+        });
+
+        secondaryLink.addEventListener('pointerleave', (e) => {
+            const theLink = e.target;
+            const topSiblingContainer = theLink.closest('ul');
+            const topSibling = topSiblingContainer.querySelector('li');
+            const topSiblingSubmenu = topSibling.querySelector('ul');
+            if (topSiblingSubmenu) {
+                topSiblingSubmenu.style.display = 'block';
+            }
+        });
+    };
+
+    /**
+     * Initializes secondary level links with event listeners.
+     */
+    const initializeSecondaryLevelLinks = () => {
+        setTimeout(() => {
             const onlySecondaryLevelLink = document.querySelectorAll(
                 'header nav.wp-block-navigation > ul.wp-block-navigation__container > li > ul.wp-block-navigation__submenu-container > li.wp-block-navigation-link > a.wp-block-navigation-item__content'
             );
+
             if (onlySecondaryLevelLink) {
                 onlySecondaryLevelLink.forEach((secondaryLink) => {
                     secondaryLink.classList.add('no-sub-menu');
-                    secondaryLink.addEventListener('pointerenter', (e) => {
-                        const theLink = e.target;
-                        const topSiblingContainer = theLink.closest('ul');
-                        const topSibling =
-                            topSiblingContainer.querySelector('li');
-                        const topSiblingSubmenu =
-                            topSibling.querySelector('ul');
-                        if (topSiblingSubmenu) {
-                            topSiblingSubmenu.style.display = 'none';
-                        }
-                    });
-
-                    secondaryLink.addEventListener('pointerleave', (e) => {
-                        const theLink = e.target;
-                        const topSiblingContainer = theLink.closest('ul');
-                        const topSibling =
-                            topSiblingContainer.querySelector('li');
-                        const topSiblingSubmenu =
-                            topSibling.querySelector('ul');
-                        if (topSiblingSubmenu) {
-                            topSiblingSubmenu.style.display = 'block';
-                        }
-                    });
+                    handleSecondaryLinkPointer(secondaryLink);
                 });
             }
         }, 50);
-    }, 0);
+    };
+
+    // Initialize all menu items and links
+    initializePrimaryMenuItems();
+    initializeSecondaryMenuItems();
+    initializeSecondaryLevelLinks();
 };
 
-if ('complete' === document.readyState) {
+// DOMContentLoaded event to ensure DOM is fully loaded
+if (document.readyState === 'complete') {
     domReady();
 } else {
     document.addEventListener('DOMContentLoaded', domReady);
