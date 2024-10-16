9899ffccbcbb5f072adcadcf6972672f86b68596
Nate King <Nate.King@gov.bc.ca>
2024-10-16 08:35:49 -0700

    Additional refactoring of JS

--- digimod-media-idir-protect/media-idir-protect.js
+++ digimod-media-idir-protect/media-idir-protect.js
@@ -1,11 +1,27 @@
+/**
+ * Placeholder image DOM manipulation for IDIR protection.
+ *
+ * @return {void}
+*/
+
 const placeholderHeight = 160;
 const placeholderWidth = 544;
+
+const domReadyForIDIRCheck = () => {
+	/*
+	 * SafarIE bug requires repaint update.
+	 */
+	window.requestAnimationFrame(() => {
+		console.log('Checking for IDIR secured images');
+		document.querySelectorAll('img').forEach(img => mediaIdirProtectHandleImage(img));
+	});
+};
 
 function mediaIdirProtectHandleImage(img) {
 	const isProtectedImage = img.src.includes('/wp-uploads-idir-protected/');
 
-	if(isProtectedImage){
-		
+	if (isProtectedImage) {
+
 		if (img.src.startsWith('http://')) {
 			img.src = img.src.replace('http://', 'https://');
 		}
@@ -14,7 +30,7 @@ function mediaIdirProtectHandleImage(img) {
 
 		fetch(urlWithNoCache, { method: 'HEAD', redirect: 'manual' }).then(response => {
 			//If we can fetch it and its not a redirect, then its not the IDIR login. Leave it alone.
-			if(response.type == 'opaqueredirect'){
+			if (response.type == 'opaqueredirect') {
 				replaceImageWithPlaceholder(img);
 			}
 
@@ -24,22 +40,24 @@ function mediaIdirProtectHandleImage(img) {
 	}
 }
 
-function replaceImageWithPlaceholder(img){
+function replaceImageWithPlaceholder(img) {
 	const placeholderSrc = `${media_idir_protect_script_vars.plugin_dir}/${media_idir_protect_script_vars.placeholder_src}`;
 	img.src = appendRandomParam(placeholderSrc);
 	setPlaceholderSize(img);
 }
 
 function setPlaceholderSize(img) {
-    img.style.width = `${placeholderWidth}px`;
-    img.style.height = `${placeholderHeight}px`;
+	img.style.width = `${placeholderWidth}px`;
+	img.style.height = `${placeholderHeight}px`;
 }
 
 function appendRandomParam(url) {
-    const timestamp = Date.now();
-    return url.includes('?') ? `${url}&_cached_at=${timestamp}` : `${url}?_cached_at=${timestamp}`;
+	const timestamp = Date.now();
+	return url.includes('?') ? `${url}&_cached_at=${timestamp}` : `${url}?_cached_at=${timestamp}`;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('img').forEach(img => mediaIdirProtectHandleImage(img));
-});
+if ('complete' === document.readyState) {
+	domReadyForIDIRCheck();
+} else {
+	document.addEventListener('DOMContentLoaded', domReadyForIDIRCheck);
+}
