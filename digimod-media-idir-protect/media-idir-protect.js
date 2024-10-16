19fe16ef9ea1161e4245e41cb563d79d2464446d
Nate King <Nate.King@gov.bc.ca>
2024-10-16 08:50:31 -0700

    Added placeholder image onload

--- digimod-media-idir-protect/media-idir-protect.js
+++ digimod-media-idir-protect/media-idir-protect.js
@@ -42,8 +42,10 @@ function mediaIdirProtectHandleImage(img) {
 
 function replaceImageWithPlaceholder(img) {
 	const placeholderSrc = `${media_idir_protect_script_vars.plugin_dir}/${media_idir_protect_script_vars.placeholder_src}`;
+	img.onload = function() {
+		setPlaceholderSize(img);
+	};
 	img.src = appendRandomParam(placeholderSrc);
-	setPlaceholderSize(img);
 }
 
 function setPlaceholderSize(img) {
