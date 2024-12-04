b420daa5cdd2229e235e374f476e2fddcce784fe
Nicolas Gomez <nicolas.gomez@gov.bc.ca>
2024-12-04 15:55:42 -0800

    github actions

--- .github/actions/test/test.sh
+++ .github/actions/test/test.sh
@@ -5,16 +5,18 @@ set -e
 ret=0
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli2.phar || ret=$?
 
-if [ ret -eq 0 ]; then
+if [ $ret -eq 0 ]; then
     # The command was successful
     echo 'Success'
+
+    chmod +x wp-cli.phar
 else
     # The command was not successful
     echo "Attempt failed. Trying again..."
 fi
 
 
-chmod +x wp-cli.phar
+
 
 echo "Done bash script"
 exit 2
