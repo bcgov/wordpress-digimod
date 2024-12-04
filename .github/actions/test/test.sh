76a1dc879e4e3946272708442ea1897eb344acd2
Nicolas Gomez <nicolas.gomez@gov.bc.ca>
2024-12-04 10:47:27 -0800

    testing github actions

--- .github/actions/test/test.sh
+++ .github/actions/test/test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+set -e
 
 #This should fail and cause the action to exit out.
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli2.phar
-#chmod +x wp-cli.phar
+
+chmod +x wp-cli.phar
 
 echo "Done bash script"
 exit 2
