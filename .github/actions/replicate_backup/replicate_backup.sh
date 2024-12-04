975ff1e4faf94098c3e8c582bf2372349d5ed12b
Nicolas Gomez <nicolas.gomez@gov.bc.ca>
2024-12-04 13:00:41 -0800

    github actions - more tweaks to improve backup. reduce backup count from 7 to 6

--- .github/actions/replicate_backup/replicate_backup.sh
+++ .github/actions/replicate_backup/replicate_backup.sh
@@ -107,8 +107,8 @@ if [ -n "$WORDPRESS_CONTAINER_NAME" ]; then
 
     # Copy over import plugins
     echo "Copying over import plugins"
-    oc cp --no-preserve ./plugins/all-in-one-wp-migration -n $NAMESPACE -c $WORDPRESS_CONTAINER_NAME $WORDPRESS_POD_NAME:/var/www/html/wp-content/plugins/all-in-one-wp-migration
-    oc cp --no-preserve ./plugins/all-in-one-wp-migration-unlimited-extension -n $NAMESPACE -c $WORDPRESS_CONTAINER_NAME $WORDPRESS_POD_NAME:/var/www/html/wp-content/plugins/all-in-one-wp-migration-unlimited-extension
+    oc cp --no-preserve ./plugins/all-in-one-wp-migration -n $NAMESPACE -c $WORDPRESS_CONTAINER_NAME $WORDPRESS_POD_NAME:/var/www/html/wp-content/plugins
+    oc cp --no-preserve ./plugins/all-in-one-wp-migration-unlimited-extension -n $NAMESPACE -c $WORDPRESS_CONTAINER_NAME $WORDPRESS_POD_NAME:/var/www/html/wp-content/plugins
     oc exec -n $NAMESPACE -c $WORDPRESS_CONTAINER_NAME $WORDPRESS_POD_NAME -- php /tmp/wp-cli.phar plugin activate all-in-one-wp-migration all-in-one-wp-migration-unlimited-extension
 
     # Copy over backup file
