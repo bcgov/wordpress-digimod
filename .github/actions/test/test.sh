f4df5b2be6ae39542a5a9c2d3a774fc4dfa30845
Nicolas Gomez <nicolas.gomez@gov.bc.ca>
2024-12-04 15:57:47 -0800

    github actions

--- .github/actions/test/test.sh
+++ .github/actions/test/test.sh
@@ -3,16 +3,31 @@ set -e
 
 #This should fail and cause the action to exit out.
 ret=0
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli2.phar || ret=$?
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli2.phar --fail || ret=$?
 
 if [ $ret -eq 0 ]; then
     # The command was successful
-    echo 'Success'
+    echo 'Success1'
 
     chmod +x wp-cli.phar
 else
     # The command was not successful
-    echo "Attempt failed. Trying again..."
+    echo "Attempt failed1. Trying again..."
+fi
+
+
+
+ret=0
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar --fail || ret=$?
+
+if [ $ret -eq 0 ]; then
+    # The command was successful
+    echo 'Success2'
+
+    chmod +x wp-cli.phar
+else
+    # The command was not successful
+    echo "Attempt failed2. Trying again..."
 fi
 
 
