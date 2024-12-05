7f98dc4a63fc1a325abab9f6ca40c451be917a5f
Nicolas Gomez <nicolas.gomez@gov.bc.ca>
2024-12-04 16:33:48 -0800

    github actions testing new OC login way

--- .github/actions/test/test.sh
+++ .github/actions/test/test.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+
+#Make sure bash exits on any error so that the github action is marked as error
 set -e
+
+OPENSHIFT_SERVER=$1
+DEV_TOKEN=$2
+TEST_TOKEN=$3
+PROD_TOKEN=$4
+
+NAMESPACE="c0cce6-prod"
+
+#testing new OC login way
+OC_TEMP_TOKEN=$(curl -k -X POST $OPENSHIFT_SERVER/api/v1/namespaces/$NAMESPACE/serviceaccounts/pipeline/token --header "Authorization: Bearer $DEV_TOKEN" -d '{"spec": {"expirationSeconds": 600}}' -H 'Content-Type: application/json; charset=utf-8' | jq -r '.status.token' )
+
+oc login --token=$OC_TEMP_TOKEN --server=$OPENSHIFT_SERVER
+oc logout
 
 #This should fail and cause the action to exit out.
 ret=0
