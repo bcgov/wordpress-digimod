9419962f057aa22ea4e863a2f15f72f86b3eeb36
Nicolas Gomez <nicolas.gomez@gov.bc.ca>
2026-08-20 13:52:36 -0700

    Work on Github Actions Backup V2

---
+++ .github/actions/list_backups_v2/list_backups_v2.sh
@@ -0,0 +1,97 @@
+
+#!/bin/bash
+
+#Make sure bash exits on any error so that the github action is marked as error
+set -e
+
+ENVIRONMENT=$1
+export PROJECT_NAME=$2
+export SITE_NAME=$3
+OPENSHIFT_SERVER=$4
+DEV_TOKEN=$5
+TEST_TOKEN=$6
+PROD_TOKEN=$7
+S3_TOKEN=$8
+
+
+echo "Backing up from environment: $ENVIRONMENT"
+case "$ENVIRONMENT" in
+    "dev")
+    token=$DEV_TOKEN
+    ;;
+    "test")
+    token=$TEST_TOKEN
+    ;;
+    "prod")
+    token=$PROD_TOKEN
+    ;;
+    *)
+    echo "Unknown environment: $ENVIRONMENT"
+    exit 1
+    ;;
+esac
+
+
+
+
+echo "Getting list of backups for ${PROJECT_NAME}"
+echo "Searching for: ${PROJECT_NAME}-${SITE_NAME}_${ENVIRONMENT}_*_backup.tar*"
+
+
+# echo "::group::Login to OC"
+#Sometimes oc login will fail to connect, so lets re-try on failure.
+# set +e
+# oc login $OPENSHIFT_SERVER --token=$PROD_TOKEN
+# ret=$?
+# set -e
+# if [ $ret -eq 0 ]; then
+#     # The command was successful
+#     echo "Login successful"
+
+# else
+#     echo "Re-trying oc-login in 10s..."
+
+#     sleep 10
+
+#     # The command was not successful, lets try again
+#     oc login $OPENSHIFT_SERVER --token=$PROD_TOKEN
+
+# fi
+
+# echo "::endgroup::"
+
+
+CMD_RESULTS=$(rclone lsf :s3:webbackup/oc-sites-bk --include "${PROJECT_NAME}-${SITE_NAME}_${ENVIRONMENT}_*_backup.tar*" --files-only --s3-provider Other --s3-access-key-id "webbkaki" --s3-secret-access-key "${{ secrets.BCGOV_S3_SECRET_ACCESS_KEY }}" --s3-endpoint "https://digital-gov.objectstore.gov.bc.ca"  --contimeout "15s" --retries 3 | sort | tail -n 20 2>&1)
+CMD_EXIT_CODE=$?
+
+echo "$CMD_RESULTS"
+
+BACKUP_LIST=""
+if [[ $CMD_EXIT_CODE -eq 0 ]]; then
+	echo "Bucket check complete."
+
+	if [ -z "$CMD_RESULTS" ]; then
+		echo "No backups found!"
+
+	else
+		BACKUP_LIST=$(sort -r <<< $CMD_RESULTS | nl)
+	fi
+
+else
+	echo "::error::Error grabbing list: ${CMD_RESULTS}"
+
+	exit 96
+fi 
+
+
+#Generate GH Actions summary
+echo "### Backups List:" >> $GITHUB_STEP_SUMMARY
+echo "" >> $GITHUB_STEP_SUMMARY # this is a blank line
+
+if [ -z "$CMD_RESULTS" ]; then
+	echo "No backups found for ${PROJECT_NAME}-${SITE_NAME}_${ENVIRONMENT}_*_backup.tar*" >> $GITHUB_STEP_SUMMARY
+
+else
+	echo "Starting at backup #1 and increasing down the list." >> $GITHUB_STEP_SUMMARY
+	echo "${BACKUP_LIST}" >> $GITHUB_STEP_SUMMARY
+fi
